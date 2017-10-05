31a695b9453e8074c4c724b1e8257dd9bba52ff0
Matt Kotsenas <matt.kotsenas@gmail.com>
2017-10-05 15:07:44 -0700

    Create skeleton /src and /test folders
    Create the skeleton /src and /test folders, with empty projects in each.

---
+++ test/RegisteredCOMLoader.Tests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("RegisteredCOMLoader.Tests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("RegisteredCOMLoader.Tests")]
+[assembly: AssemblyCopyright("Copyright ©  2017")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("4b8e0413-3d0d-4898-9f1a-a5480ae4c757")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
