483ae51e18775407b8d78cca6f67ce4c1b2d6816
Matt Kotsenas <matt.kotsenas@gmail.com>
2018-03-03 18:12:42 -0800

    Fix bug when passing a relative path to to UnregisteredClassLoader
    The `UnregisteredClassLoader` uses `LoadLibrary` under the hood, which has
    known issues with passing in relative paths. Fix that issue by getting the
    absolute path before passing to `LoadLibrary`.
    
    Adding a unit test caught an additional bug where the loader wasn't
    holding on to the `SafeHandle`, which could cause the library to stay
    loaded and / or get unloaded from underneath the user. The fix is to make
    the loader hold on to the `SafeHandle`, and implement `IDisposable`.

--- src/UnregisteredClassLoader.cs
+++ src/UnregisteredClassLoader.cs
@@ -4,10 +4,29 @@ using PInvoke;
 
 namespace COMClassLoader
 {
-    public class UnregisteredClassLoader : IClassLoader
+    // TODO: Since this loader implements IDispoable, it should be added to the IClassLoader interface instead
+    public class UnregisteredClassLoader : IClassLoader, IDisposable
     {
         private delegate int DllGetClassObject(ref Guid classId, ref Guid interfaceId, [Out, MarshalAs(UnmanagedType.Interface)] out object unknownObject);
         private readonly string _path;
+        private Kernel32.SafeLibraryHandle _library;
+
+        protected void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                if (_library != null && !_library.IsClosed)
+                {
+                    _library.Dispose();
+                }
+            }
+        }
+
+        public void Dispose()
+        {
+            Dispose(true);
+            GC.SuppressFinalize(this);
+        }
 
         public UnregisteredClassLoader(string path)
         {
@@ -21,12 +40,15 @@ namespace COMClassLoader
                 throw new ArgumentException($"Type {typeof(T)} is not a COM object");
             }
 
+            var path = EnsureLibraryExists(_path);
+
             var classFactoryId = typeof(IClassFactory).GUID;
             var classId = typeof(T).GUID;
             var interfaceId = typeof(TResult).GUID;
 
-            var handle = Kernel32.LoadLibrary(_path);
-            var dllGetClassObjectPtr = Kernel32.GetProcAddress(handle, "DllGetClassObject");
+            _library = EnsureLibraryLoaded(path);
+
+            var dllGetClassObjectPtr = Kernel32.GetProcAddress(_library, "DllGetClassObject");
             var dllGetClassObjectDelegate = Marshal.GetDelegateForFunctionPointer<DllGetClassObject>(dllGetClassObjectPtr);
 
             dllGetClassObjectDelegate(ref classId, ref classFactoryId, out var classFactoryAsUnknown);
@@ -34,6 +56,26 @@ namespace COMClassLoader
 
             classFactory.CreateInstance(null, ref interfaceId, out var resultAsUnknown);
             return resultAsUnknown as TResult;
+        }
+
+        private static string EnsureLibraryExists(string path)
+        {
+            var file = new System.IO.FileInfo(path);
+            if (!file.Exists)
+            {
+                throw new ArgumentException($"The path {path} does not exist");
+            }
+            return file.FullName;
+        }
+
+        private static Kernel32.SafeLibraryHandle EnsureLibraryLoaded(string path)
+        {
+            var library = Kernel32.LoadLibrary(path);
+            if (library.IsInvalid)
+            {
+                throw new TypeLoadException($"LoadLibrary of library {path} failed");
+            }
+            return library;
         }
     }
 }
--- test/UnregisteredCOMLoader.Tests/UnregisteredCOMLoader.Tests.cs
+++ test/UnregisteredCOMLoader.Tests/UnregisteredCOMLoader.Tests.cs
@@ -1,19 +1,43 @@
-﻿using COMClassLoader;
+﻿using System.IO;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using COMClassLoader;
 using UnregisteredCOMLoader.Tests.Mocks;
 
 namespace UnregisteredCOMLoader.Tests
 {
     [TestClass]
-    public class given_an_unregistered_COM_object
+    public class given_an_unregistered_COM_object_with_an_absolute_path
     {
         [TestMethod]
         [DeploymentItem(@"Mocks\MyComClass.dll")]
         [DeploymentItem("Microsoft.VisualStudio.TestPlatform.TestFramework.Extensions.dll")] // https://github.com/Microsoft/testfx/issues/91
         public void it_can_be_instantiated_through_the_dllclassobject()
         {
-            var myComObject = new UnregisteredClassLoader("MyComClass.dll").Load<MyComEntryClass, IMyComInterface>();
-            Assert.IsNotNull(myComObject);
+            var path = Path.Combine(System.Environment.CurrentDirectory, "MyComClass.dll");
+
+            using (var loader = new UnregisteredClassLoader(path))
+            {
+                var myComObject = loader.Load<MyComEntryClass, IMyComInterface>();
+                Assert.IsNotNull(myComObject);
+            }
+        }
+    }
+
+    [TestClass]
+    public class given_an_unregistered_COM_object_with_a_relative_path
+    {
+        [TestMethod]
+        [DeploymentItem(@"Mocks\MyComClass.dll")]
+        [DeploymentItem("Microsoft.VisualStudio.TestPlatform.TestFramework.Extensions.dll")] // https://github.com/Microsoft/testfx/issues/91
+        public void it_can_be_instantiated_through_the_dllclassobject()
+        {
+            var path = "MyComClass.dll";
+
+            using (var loader = new UnregisteredClassLoader(path))
+            {
+                var myComObject = loader.Load<MyComEntryClass, IMyComInterface>();
+                Assert.IsNotNull(myComObject);
+            }
         }
     }
 }
